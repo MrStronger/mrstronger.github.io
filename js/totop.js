10691ba69b38fce27d308f61f46251effbbbce24
MrStronger <1274552194@qq.com>
2017-10-20 21:42:15 +0800

    Site updated: 2017-10-20 21:42:15

--- js/totop.js
+++ js/totop.js
@@ -16,7 +16,7 @@ window.onscroll = function() {
   var logoImg = document.getElementsByClassName("logoImg")[0]
   var name = document.getElementById('hidden')
 
-  var logoImgLeft = parseInt(window.getComputedStyle(logoImg).offsetLeft)
+  var logoImgLeft = logoImg.offsetLeft
   console.log(logoImgLeft)
   if (document.body.clientWidth < 500 && logoImgLeft > 10) {
     logoImg.style.transform = 'translate(' + -scroll + 'px,0)'
