2b5d5c8111fe3b0b66aeac0972f0a90895b2efce
MrStronger <1274552194@qq.com>
2017-10-21 09:24:45 +0800

    Site updated: 2017-10-21 09:24:44

--- js/totop.js
+++ js/totop.js
@@ -16,9 +16,9 @@ window.onscroll = function() {
   var logoImg = document.getElementsByClassName("logoImg")[0]
   var name = document.getElementById('hidden')
 
-  var logoImgLeft = logoImg.offsetLeft
+  var logoImgLeft = document.body.clientWidth/2 - 34
   console.log(logoImgLeft)
-  if (document.body.clientWidth < 500 && logoImgLeft > 10) {
+  if (document.body.clientWidth < 500 && logoImgLeft > 0) {
     logoImg.style.transform = 'translate(' + -scroll + 'px,0)'
   }
   if (scroll >= 300) {
